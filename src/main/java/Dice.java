3cf2e1ac5e0b2ef00672165ba905e9bf488c44d9
AmiralBl3ndic <camille.briand@efrei.net>
2018-08-29 20:52:46 -0400

    Add Dice class

---
+++ src/main/java/Dice.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class Dice {
+	// TODO: implement method 'roll'
+	
+	/**
+	 * Rolls a dice
+	 * @return An integer between 1 (included) and 6 (included)
+	 */
+	public static int roll () {
+		return 0;
+	}
+}
