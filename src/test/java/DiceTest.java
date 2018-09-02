d44d9b30e9b8ba4bf09ffb401962af087522da85
AmiralBl3ndic <camille.briand@efrei.net>
2018-09-02 17:02:53 -0400

    Modify DiceTest class to use the new Dice API

--- src/test/java/DiceTest.java
+++ src/test/java/DiceTest.java
@@ -8,9 +8,11 @@ class DiceTest {
 	
 	@org.junit.jupiter.api.Test
 	void roll() {
+		Dice dice = new Dice();
+		
 		// Since this method involves random numbers, we run it a sufficient number of times to ensure it works
 		for (int i = 0; i < 500; i++) {
-			int val = Dice.roll();
+			int val = dice.roll();
 			
 			assertTrue(val <= 6, "should be smaller or equal to 6");
 			assertTrue(val >= 1, "should be greater or equal to 1");
