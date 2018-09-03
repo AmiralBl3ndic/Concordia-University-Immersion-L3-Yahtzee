98fc0d5f8afdc22d50c418efd81b8e77452a1252
AmiralBl3ndic <camille.briand@efrei.net>
2018-09-03 12:07:27 -0400

    Add `lock` and `unlock` methods to `Dice` class

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -21,4 +21,23 @@ public class Dice {
 	
 		return value;
 	}
+	
+	
+	/**
+	 * Locks the dice
+	 * Calling this method will prevent the dice from being rolled
+	 * When a dice is locked using this method, calling the `roll` method won't roll the dice and it will keep its value until unlocked
+	 */
+	public void lock () {
+		locked = true;
+	}
+	
+	
+	/**
+	 * Unlocks the dice
+	 * When a dice is locked, it won't roll even if the `roll` method is called. Calling the `unlock` method will unlock the dice
+	 */
+	public void unlock () {
+	
+	}
 }
